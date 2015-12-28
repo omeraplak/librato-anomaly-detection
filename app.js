9ba63278589674ff265ae5612a3d189f6cd9bb0a
Omer <omer@Omers-MacBook-Pro.local>
2015-12-28 15:03:58 +0200

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+var CronJob = require('cron').CronJob;
+var metric = require('./lib/metric');
+var metrics = require('./metrics.json');
+
+function Do(){
+    for(var i = 0; i < metrics.length; i++){
+        metric.check(metrics[i]);
+    }
+}
+
+
+new CronJob('* * */1 * *', function () {
+   Do();
+}, null, true);
