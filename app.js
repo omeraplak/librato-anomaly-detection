5f90c19ef474c9ca7d2932693e4231ab72bc59cf
Ali Barın <ali.barin53@gmail.com>
2015-12-28 17:22:02 +0200

    Add pass config support via CLI

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ var metric = require('./lib/metric');
 var metrics = require('./metrics.json');
 
 function Do(){
-    for(var i = 0; i < metrics.length; i++){
-        metric.check(metrics[i]);
-    }
+  for(var i = 0; i < metrics.length; i++){
+    metric.check(metrics[i]);
+  }
 }
 
-
 new CronJob('* * */1 * *', function () {
-   Do();
+  Do();
 }, null, true);
